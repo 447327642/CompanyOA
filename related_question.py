d10343a5b9a7aa3a2327430be26b2c85e2d2a9cc
WANG Cheng <wcyz666@126.com>
2015-10-19 00:38:12 -0700

    Finalized version of relate question

--- related_question.py
+++ related_question.py
@@ -1,56 +1,68 @@
-# I was rejected in resume screening phase, so if you needed me to solve more
-# problems in order to get a phone interview, please let me know, and I will
-# try my best to solve them.
+# If you needed me to solve more problems in order to get
+# a phone interview, please let me know, and I will try my
+# best to solve them.
 
-__author__ = 'WANG. Cheng'
+# This algorithm works like an inorder traversal iteratively
+# Once a node is computed, its value is memorized such that
+# we don't need to compute it again.
 
 
-def save_relation(adj_list, nodeA, nodeB):
-    if nodeA in adj_list:
-        adj_list[nodeA]['adjacents'].append(nodeB)
+def save_relation(_adj_list, node_a, node_b):
+    if node_a in _adj_list:
+        _adj_list[node_a]['neighbors'].append(node_b)
     else:
-        adj_list[nodeA] = {
-            'adjacents': [nodeB],
+        _adj_list[node_a] = {
+            'neighbors': [node_b],
             'memo': {}
         }
 
 
-def bfs_with_memo(adj_list, read_time, start):
+def inorder_with_memo(adj_list, read_time, start):
 
     node_stack = []
-    prev_stack = []
     cur = start
-    cur_index = 0
     prev = None
-    sum = 0.0
 
-    while len(node_stack) or cur_index < len(adj_list[cur]["adjacents"]):
-        adjacents = adj_list[cur]["adjacents"]
-        while (prev and len(adjacents) >= 2) or (not prev):
+    while True:
+        neighbors = adj_list[cur]["neighbors"]
+        memo = adj_list[cur]["memo"]
+
+        # See whether this branch has been cached, if not, we go into this branch
+        # and push the parent node to the stack
+        while (prev not in memo) and len(neighbors) >= 2:
             sum = 0.0
-            cur_index = 1 if adjacents[0] == prev else 0
-            node_stack.append((prev, cur, cur_index, len(adjacents), sum))
+            cur_index = 1 if neighbors[0] == prev else 0
+            node_stack.append((prev, cur, cur_index, len(neighbors), sum))
             prev = cur
-            cur = adjacents[cur_index]
-            adjacents = adj_list[cur]["adjacents"]
+            cur = neighbors[cur_index]
+            neighbors = adj_list[cur]["neighbors"]
+            memo = adj_list[cur]["memo"]
         else:
-            tmp_cur = cur
+            # One of the branches reaches the end. We pop the parent node and add the readtime
+            # to the parent node
+            sum = (read_time[cur - 1] if prev not in memo else memo[prev])
             (prev, cur, cur_index, n_count, prev_sum) = node_stack.pop()
-            sum = prev_sum + read_time[tmp_cur - 1]
-            while cur_index == n_count - 1 or (cur_index == n_count - 2 and adj_list[cur]["adjacents"][-1] == prev):
-                if not prev:
-                    return sum / n_count + read_time[cur - 1]
+            sum += prev_sum
+
+            # Check if all children have been visited. If so, we pop the parent node, update the sum, and
+            # cache the result. If not, we move to the next child.
+            while cur_index == n_count - 1 or (cur_index == n_count - 2 and adj_list[cur]["neighbors"][-1] == prev):
                 sum = sum / (n_count - 1) + read_time[cur - 1]
+                if not prev:
+                    return sum
+
                 adj_list[cur]['memo'][prev] = sum
+
                 (prev, cur, cur_index, n_count, prev_sum) = node_stack.pop()
                 sum += prev_sum
             else:
-                cur_index = cur_index + 2 if adj_list[cur]["adjacents"][cur_index + 1] == prev else cur_index + 1
-                node_stack.append((prev, cur, cur_index, len(adj_list[cur]["adjacents"]), sum))
+                neighbors = adj_list[cur]["neighbors"]
+                cur_index = cur_index + 2 if neighbors[cur_index + 1] == prev else cur_index + 1
+                node_stack.append((prev, cur, cur_index, n_count, sum))
                 prev = cur
-                cur = adj_list[cur]["adjacents"][cur_index]
+                cur = neighbors[cur_index]
 
-
+# The graph(tree) is stored using adjacent list.
 if __name__ == "__main__":
 
     q_count = int(raw_input())
@@ -66,12 +78,13 @@ if __name__ == "__main__":
     opt_start = -1
     min_time = -1
     for i in range(q_count):
-        ith_result = bfs_with_memo(adj_list, q_read_time, i + 1)
-        print ith_result
-        if min_time == -1 or min_time > ith_result:
-            opt_start = i + 1
-            min_time = ith_result
-
+        if (len(adj_list[i + 1]["neighbors"]) > 1):
+            adj_list[i + 1]["neighbors"].append(None)
+            ith_result = inorder_with_memo(adj_list, q_read_time, i + 1)
+            if min_time == -1 or min_time > ith_result:
+                opt_start = i + 1
+                min_time = ith_result
+            adj_list[i + 1]["neighbors"].pop()
+            #print ith_result
 
     print opt_start
-    print adj_list
